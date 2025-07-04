bfea24e68f564fe5e9c179377b680438e4bd5b49
JoseLuisAlvarezManica <114442646+JoseLuisAlvarezManica@users.noreply.github.com>
2025-07-03 22:21:23 -0600

    Prueba conexion azure 4

--- api/analyze.js
+++ api/analyze.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
 
       const location = azureRes.headers.get("operation-location");
       if (!location) {
-        return res.status(500).json({ error: "Missing operation-location" });
+        return res.status(500).json({ error: "Missing operation-location in Azure response" });
       }
 
       return res.status(200).json({ operationLocation: location });
