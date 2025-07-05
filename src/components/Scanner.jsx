6b1ccd40a53d7e63347e5d580030d8bdbb34200e
JoseLuisAlvarezManica <114442646+JoseLuisAlvarezManica@users.noreply.github.com>
2025-07-04 23:10:39 -0600

    Fix visual 13

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -420,7 +420,7 @@ export const Scanner = () => {
                     console.error("Error uploading data: ", error);
                     alert("Error uploading data.");        
                 }}}
-                className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-blue-700 transition-colors"
+                className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 transition-colors"
               >
                 Subir a base de datos
               </button>
